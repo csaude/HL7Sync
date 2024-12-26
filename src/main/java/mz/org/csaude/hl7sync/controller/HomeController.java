f1bf41dfe5227786c01f26eec85f2f1ee727c110
Shaquil Hanif <shaquil.hanif@csaude.org.mz>
2024-12-26 12:38:30 +0200

    Initial commit for Spring Boot project

---
+++ src/main/java/mz/org/csaude/hl7sync/controller/HomeController.java
@@ -0,0 +1,15 @@
+package mz.org.csaude.hl7sync.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "<h1>Welcome to Spring Boot</h1>";
+    }
+}
+
---
+++ src/test/java/mz/org/csaude/hl7sync/Hl7syncApplicationTests.java
@@ -0,0 +1,13 @@
+package mz.org.csaude.hl7sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hl7syncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
