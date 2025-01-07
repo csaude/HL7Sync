8d4fc8765270c80b1a5aa2f87ea9eb4324221c35
Shaquil Hanif <shaquil.hanif@csaude.org.mz>
2025-01-07 11:20:55 +0200

    Saving progress

--- api/src/main/java/mz/org/csaude/hl7sync/model/Location.java
+++ api/src/main/java/mz/org/csaude/hl7sync/model/Location.java
@@ -12,11 +12,11 @@ public class Location implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-//    public static String joinLocations(List<Location> locations) {
-//        return locations.stream()
-//                .map(Location::getName)
-//                .collect(Collectors.joining(" • "));
-//    }
+    public static String joinLocations(List<Location> locations) {
+        return locations.stream()
+                .map(Location::getName)
+                .collect(Collectors.joining(" • "));
+    }
 
     private String uuid;
 
