fd61125709ef335a2f4543e078388bd27fdf6b66
Shaquil Hanif <shaquil.hanif@gmail.com>
2025-01-08 16:33:43 +0200

    Saving progress 16:33

--- api/src/main/java/mz/org/csaude/hl7sync/controller/ApiController.java
+++ api/src/main/java/mz/org/csaude/hl7sync/controller/ApiController.java
@@ -1,67 +1,59 @@
 package mz.org.csaude.hl7sync.controller;
 
-
 import ca.uhn.hl7v2.HL7Exception;
 import mz.org.csaude.hl7sync.model.HL7FileRequest;
 import mz.org.csaude.hl7sync.model.Location;
+import mz.org.csaude.hl7sync.model.PatientDemographic;
 import mz.org.csaude.hl7sync.service.Hl7Service;
 import mz.org.csaude.hl7sync.service.LocationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
     private static final Logger LOG = LoggerFactory.getLogger(ApiController.class);
 
     private Hl7Service hl7Service;
+    private HL7FileGeneratorDao hl7FileGeneratorDao
     private LocationService locationService;
 
-    public ApiController(Hl7Service hl7Service, LocationService locationService){
+    public ApiController(Hl7Service hl7Service, LocationService locationService) {
         this.hl7Service = hl7Service;
         this.locationService = locationService;
     }
-    @GetMapping("/demographics/generate")
-    public void createHL7Request() throws HL7Exception, IOException {
+
+    @PostMapping("/demographics/generate")
+    public ResponseEntity<?> createHL7Request(@RequestParam String locationUUID) throws HL7Exception, IOException {
         HL7FileRequest req = new HL7FileRequest();
 
-        List<Location> allProvinces = locationService.findAllProvinces();
-
-        Location province = allProvinces.get(0);
-
-
+        Location province = locationService.findByUuid(locationUUID);
         req.setProvince(province);
-
-//        System.out.println(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e"));
-
-//        req.setDistrict(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e").getChildLocations().get(0));
         req.setDistrict(province.getChildLocations().get(0));
-
-        System.out.println("Starting 3");
-
-//        req.setHealthFacilities(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e").getChildLocations());
         req.setHealthFacilities(province.getChildLocations());
 
+        List<String> locationsByUuid = req.getHealthFacilities().stream()
+                .map(Location::getUuid)
+                .collect(Collectors.toList());
 
+        List<PatientDemographic> patientDemographics = hl7FileGeneratorDao.getPatientDemographicData(locationsByUuid);
+        LOG.info("Query executed successfully");
 
-//        req.setProvince(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab"));
-//        System.out.println(req.getProvince());
-//        req.setDistrict(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations().get(0));
-//       System.out.println(req.getDistrict());
-//        req.setHealthFacilities(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations());
-//        System.out.println(req.getHealthFacilities());
-//
-        hl7Service.generateHl7File(req);
-       System.out.println("Done");
-        // ConfigController.previousHl7FileForm = hl7FileForm;
+        // Prepare the response
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "HL7 file request generated successfully");
+        response.put("patientDemographics", patientDemographics);
 
-        LOG.info("File generated");
+        // Return a 200 OK status with the response body
+        return ResponseEntity.ok(response);
     }
 }
