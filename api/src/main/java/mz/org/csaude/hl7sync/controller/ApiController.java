5ed089007a7155d34ec1173f97ce676a768cea47
Shaquil Hanif <shaquil.hanif@gmail.com>
2025-01-08 14:57:18 +0200

    Saving progress

--- api/src/main/java/mz/org/csaude/hl7sync/controller/ApiController.java
+++ api/src/main/java/mz/org/csaude/hl7sync/controller/ApiController.java
@@ -3,6 +3,7 @@ package mz.org.csaude.hl7sync.controller;
 
 import ca.uhn.hl7v2.HL7Exception;
 import mz.org.csaude.hl7sync.model.HL7FileRequest;
+import mz.org.csaude.hl7sync.model.Location;
 import mz.org.csaude.hl7sync.service.Hl7Service;
 import mz.org.csaude.hl7sync.service.LocationService;
 import org.slf4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,12 +31,33 @@ public class ApiController {
     @GetMapping("/demographics/generate")
     public void createHL7Request() throws HL7Exception, IOException {
         HL7FileRequest req = new HL7FileRequest();
-        req.setProvince(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab"));
-        System.out.println(req.getProvince());
-        req.setDistrict(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations().get(0));
-       System.out.println(req.getDistrict());
-        req.setHealthFacilities(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations());
-        System.out.println(req.getHealthFacilities());
+
+        List<Location> allProvinces = locationService.findAllProvinces();
+
+        Location province = allProvinces.get(0);
+
+
+        req.setProvince(province);
+
+//        System.out.println(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e"));
+
+//        req.setDistrict(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e").getChildLocations().get(0));
+        req.setDistrict(province.getChildLocations().get(0));
+
+        System.out.println("Starting 3");
+
+//        req.setHealthFacilities(locationService.findByUuid("9b94c91a-904f-4038-89c2-f967a7f1476e").getChildLocations());
+        req.setHealthFacilities(province.getChildLocations());
+
+
+
+//        req.setProvince(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab"));
+//        System.out.println(req.getProvince());
+//        req.setDistrict(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations().get(0));
+//       System.out.println(req.getDistrict());
+//        req.setHealthFacilities(locationService.findByUuid("b637a1f8-21a6-4b75-8a66-73626903deab").getChildLocations());
+//        System.out.println(req.getHealthFacilities());
+//
         hl7Service.generateHl7File(req);
        System.out.println("Done");
         // ConfigController.previousHl7FileForm = hl7FileForm;
