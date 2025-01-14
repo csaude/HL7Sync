36fb5c3723a4ece453e414d31b56acbb2d1b8631
Shaquil Hanif <shaquil.hanif@gmail.com>
2025-01-14 12:46:17 +0200

    Saving progress 12:46

---
+++ api/src/main/java/mz/org/csaude/hl7sync/dao/JobRepositoryDao.java
@@ -0,0 +1,13 @@
+package mz.org.csaude.hl7sync.dao;
+
+import mz.org.csaude.hl7sync.model.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface JobRepositoryDao extends JpaRepository<Job, Long> {
+    Optional<Job> findByLocationUUIDAndStatusIn(String locationUUID, List<Job.JobStatus> statuses);
+}
